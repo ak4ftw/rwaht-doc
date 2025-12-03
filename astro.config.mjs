22ce973263b892bac4c019cdfd4ed7f9948668ab
ak4ftw <ak490216135@gmail.com>
2025-12-03 19:31:22 +0800

    修改 部分内容

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'RWAHT 官方文档 Q&A',
+            favicon: '/favicon.ico',
 			//social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 
             locales: {
