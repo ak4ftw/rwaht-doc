e414b82942829bce614adf5ec28d8c7ea3f90010
ak4ftw <ak490216135@gmail.com>
2025-12-03 18:51:06 +0800

    修改 部分内容

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,19 +7,31 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'RWAHT 官方文档 Q&A',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			//social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+
+            locales: {
+                root: {
+                    label: '简体中文',
+                    lang: 'zh-CN',
+                },
+            },
+
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+                {
+                    label: '开始',
+                    items: [
+                        { label: '官方媒体地址', slug: 'start/official-address' },
+                        { label: '如何获取手机app', slug: 'start/how-to-get-app' },
+                        { label: '为什么我的提币未到账', slug: 'start/why-has-not-my-withdrawal-arrived-in-the-account' },
+                        { label: '充/提币现到错误地址，可以找回吗', slug: 'start/translate-error-address' },
+                    ],
+                },
+                {
+                    label: '商务合作',
+                    items: [
+                        { label: '如何让我的代币在RWAHT中交易', slug: 'bisiness/how-to-trade-my-token-in-rwaht' },
+                    ],
+                },
 			],
 		}),
 	],
---
+++ src/content/docs/start/official-address.md
@@ -0,0 +1,19 @@
+---
+title: RWAHT 官方网站地址是多少
+description: RWAHT 官方网站地址是多少
+---
+
+## 官方信息
+
+网址: https://rwaht.com
+
+谷歌商店: [点击访问谷歌商店](https://play.google.com/store/apps/details?id=com.quyum.rwaexchange)
+
+邮箱: rwaht2025@gmail.com
+
+客服: [点击打开客服](#)
+
+TG群: https://t.me/rwahtex
+
+X: https://x.com/rwahtex
+
